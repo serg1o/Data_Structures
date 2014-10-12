a50bc81bf62a881d4d105960eaeca63524d5df75
Sérgio Ribeiro <sergiomsr@gmail.com>
2014-10-12 14:16:25 +0100

    optimization of knight.rb

--- knight.rb
+++ knight.rb
@@ -12,10 +12,10 @@ class Board
   attr_accessor :squares, :hypo_moves
 
   def initialize
-    @squares = [[]]
+    @squares = []
     (0..7).each do |y|
-      (0..7).each { |x| @squares[y].push(Square.new(x,y)) }
       @squares.push([])
+      (0..7).each { |x| @squares[y].push(Square.new(x,y)) }
     end
     @hypo_moves = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]]
     build_graph
@@ -31,13 +31,6 @@ class Board
     end
   end
 
-  def show_board
-    (0..7).each do |y|
-      (0..7).each { |x| puts "x: " + x.to_s + " y: " + y.to_s + " -> " + @squares[x][y].connections.inspect }
-      puts "================================="
-    end
-  end
-
   def breadth_first_search(origin, target)
     queue = [origin] 
     return squares[origin[0]][origin[1]] if ((target[0] == origin[0]) && (target[1] == origin[1]))
@@ -46,17 +39,15 @@ class Board
       square_coord = queue.pop
       children = squares[square_coord[0]][square_coord[1]].connections
       children.each do |next_child|
-        if !next_child.nil?
-          queue.unshift(next_child)
-          came_from[next_child] = square_coord if !came_from.has_key?(next_child)
-          return came_from if ((target[0] == next_child[0]) && (target[1] == next_child[1]))
-        end
+        queue.unshift(next_child)
+        came_from[next_child] = square_coord if !came_from.has_key?(next_child)
+        return came_from if ((target[0] == next_child[0]) && (target[1] == next_child[1]))
       end
     end
   end
 
   def knight_moves(from, to)
-    return from if from == to
+    return [from] if from == to
     paths = breadth_first_search(from, to)
     shortest_path = [to]
     back_step = paths[to]
@@ -69,20 +60,28 @@ class Board
 
 end
 
+def show_moves(moves)
+  puts "\nGoing from #{moves.first} to #{moves.last}." 
+  puts "You made it in #{moves.count - 1} moves!  Here's your path:\n"
+  moves.each do |m|
+    print "#{m}"
+    print " -> " unless m == moves.last
+  end
+  puts
+end
 
 c = Board.new
 
-puts c.knight_moves([0,0], [3,3]).inspect
-puts c.knight_moves([0,0], [1,2]).inspect
-puts c.knight_moves([3,3], [0,0]).inspect
-puts c.knight_moves([3,3], [4,3]).inspect
-puts c.knight_moves([3,3], [3,3]).inspect
-puts c.knight_moves([0,0], [7,7]).inspect
-puts c.knight_moves([4,4], [3,3]).inspect
-
-puts c.knight_moves([1,6], [6,1]).inspect
-puts c.knight_moves([3,3], [7,6]).inspect
-puts c.knight_moves([1,0], [6,0]).inspect
+show_moves(c.knight_moves([0,0], [3,3]))
+show_moves(c.knight_moves([0,0], [1,2]))
+show_moves(c.knight_moves([3,3], [0,0]))
+show_moves(c.knight_moves([3,3], [4,3]))
+show_moves(c.knight_moves([0,5], [6,6]))
+show_moves(c.knight_moves([0,0], [7,7]))
+show_moves(c.knight_moves([4,4], [3,3]))
+show_moves(c.knight_moves([1,6], [6,1]))
+show_moves(c.knight_moves([3,3], [7,6]))
+show_moves(c.knight_moves([1,0], [6,0]))
 
 
 #moves_count = []
